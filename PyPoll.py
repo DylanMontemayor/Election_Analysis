802a66b32bfd62c60ab9cd3744b9b8661a771e76
DylanMontemayor <dylang.montemayor@gmail.com>
2022-07-18 13:42:08 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,12 +1,3 @@
-# The data we need to retrieve
-
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote. 
-
-#Import the datetime class from the datatime module
 import datetime
 import csv
 import os
@@ -17,31 +8,42 @@ now = datetime.datetime.now()
 #Print the present time
 print("The time right now is", now)
 
-#Method1
-# c --file_to_load = 'Resources/election_results.csv'
-#Open the election results and read the file
-# c --election_data = open(file_to_load,'r')
-#To do: perform analysis.
-
-#Close the file
-# c --election_data.close()
-
-#Method2
-#Open the election results and read the file
-# c --with open(file_to_load) as election_data:
-#To do: perform analysis.
-    # c --print(election_data)
-
 #finalcode
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Election_Analysis","Resources","election_results.csv")
 # Open the election results and read the file.
+total_votes = 0
+candidate_options=[]
+candidate_votes = {}
+vote_percentage=0
+winning_candidate=""
+winning_count=0
+winning_percentage=0
 with open(file_to_load) as election_data:
     print(election_data)
     #To do
     file_reader=csv.reader(election_data)
-    for row in next(file_reader):
-        print(row)
+    headers = next(file_reader)
+    for row in file_reader:
+        total_votes = total_votes + 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name]+=1
+    for candidate_name in candidate_votes:
+        vote_percentage = float(candidate_votes[candidate_name]/total_votes*100)
+        print(f"{candidate_name}: {vote_percentage:.2f}% ({candidate_votes[candidate_name]:,})")
+        if (candidate_votes[candidate_name] > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = candidate_votes[candidate_name]
+            winning_percentage = vote_percentage
+            winning_candidate=candidate_name
+    candidate_summary = (f"\nWinner: {winning_candidate}\nWinning Vote Count: {winning_count:,}\nWinning Percentage: {winning_percentage:.1f}%\n")
+    print(total_votes)
+    print(candidate_options)
+    print(candidate_votes)
+    print(candidate_summary)
+
 
 
     # __Read and print the header row.
@@ -49,7 +51,7 @@ with open(file_to_load) as election_data:
     #print(headers)
 
 # Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("Election_Analysis","analysis", "election_analysis.txt")
+file_to_save = os.path.join("Election_Analysis","Analysis", "election_analysis.txt")
 # Using the open() function with the "w" mode we will write data to the file.
 open(file_to_save, "w")
 
